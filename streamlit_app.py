fe6b9b7bec827c1b49a39a4331fe4d7757d0ef0e
MR3876 Account <108825585+MR3876-appdevelper@users.noreply.github.com>
2022-07-08 04:58:23 -0700

    Update streamlit_app.py
    added header
    added 3 lines of text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My first web data application')
+Streamlit.header ('Application Header')
+Streamlit.text('Test line one')
+Streamlit.text('Test line 2')
+Streamlit.text('Test line thrice')
