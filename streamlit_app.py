350149729ba28f22c88323f92ac509189e0fd54a
MR3876 Account <108825585+MR3876-appdevelper@users.noreply.github.com>
2022-07-08 05:15:17 -0700

    Update streamlit_app.py
    added panda library
    added my_fruit_list dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,7 @@ streamlit.text('✌Test line 2')
 streamlit.text('🤞Test line thrice')
 
 streamlit.header ('🎶Get some data🤦‍♀️')
+
+import pandas
+my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
